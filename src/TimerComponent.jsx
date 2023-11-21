e9193446835e3e6d3d482c5215b2c12227c2acb6
AR-public <khayyamraja96@gmail.com>
2023-11-21 22:40:40 +0000

    Initialise useEffect hook to render the timer display.

--- src/TimerComponent.jsx
+++ src/TimerComponent.jsx
@@ -1,5 +1,4 @@
-import React from "react";
-import { useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 
 export default function TimerComponent(buttonReadsStart) {
 
@@ -9,44 +8,29 @@ export default function TimerComponent(buttonReadsStart) {
   let startingNumberofMiliseconds = startingNumberofMinutes * 60000
 
   // Initialise States
-  const [totalMinutesRemaining, setTotalMinutesRemaining] = useState(Math.floor(startingNumberofMinutes))
+  let [totalMinutesRemaining, setTotalMinutesRemaining] = useState(startingNumberofMinutes)
+  let [totalSecondsRemaining, setTotalSecondsRemaining] = useState(startingNumberofSeconds)
+  // let [totalMilisecondsRemaining, setTotalMilisecondsRemaining] = useState(startingNumberofMiliseconds)
 
-  const [totalSecondsRemaining, setTotalSecondsRemaining] = useState(startingNumberofSeconds)
+  let totalMilisecondsRemaining = useRef(startingNumberofMiliseconds)
 
   // Pad display digits to always show two numbers
   let numberOfSecondsRemainingToDisplay = String(totalSecondsRemaining).padStart(2, '0')
-
   let numberOfMinutesRemainingToDisplay = String(totalMinutesRemaining).padStart(2, '0')
 
+  // Functions
 
-  // Functions to run every milisecond/second
-
-  function commenceCountdown() { setInterval((startingNumberofMiliseconds--, console.log(startingNumberofMiliseconds)), 1) }
-
-  // This needs a watcher of some kind
-  buttonReadsStart ? clearInterval(commenceCountdown) : commenceCountdown()
+  // useEffect() runs when anything in the DOM re-renders
+  useEffect(() => {
+    const interval = setInterval(() => { (totalMilisecondsRemaining.current = totalMilisecondsRemaining - 1000) }, 1000);
+    setTotalMinutesRemaining(Math.floor(totalMilisecondsRemaining / 60000));
+    setTotalSecondsRemaining(Math.floor(totalMilisecondsRemaining / 1000));
+    return () => clearInterval(interval);
+  }, [totalMilisecondsRemaining, startingNumberofMiliseconds]);
 
   return (
-    <div className="TimerComponent" onClick={() => commenceCountdown()}>
+    <div className="TimerComponent">
       <h1>{numberOfMinutesRemainingToDisplay}:{numberOfSecondsRemainingToDisplay}</h1>
     </div>
   )
 }
-
-// NEXT STEPS - UNDERSTAND HOW TO IMPLEMENT SET_INTERVAL AND UPDATE THE VALUES FOR THE DISPLAY MINUTES AND SECONDS
-
-// LOGIC OF TIMER:
-//
-// 1. Ask user to input an integer (number of minutes)
-
-// 2. Convert the minutes into miliseconds (this is the total time you have to play with)
-
-// 3. Every second, subtract 1000 miliseconds from this total time value
-
-// 4. Every second, divide the total time by 60,000 and round down to the nearest integer to find the TOTAL NUMBER OF MINUTES
-
-// 5. Every second, subtract the total time by (TOTAL NUMBER OF MINUTES * 60,000), divide by 1000 and round down to the nearest integer to find the TOTAL NUMBER OF SECONDS
-
-// 6. Once the TOTAL NUMBER OF MINUTES and TOTAL NUMBER SECONDS = 0, stop the timer
-
-// 7. Every second, render the total number of minutes and seconds
