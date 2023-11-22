48d0e6f0b6dae534257c5f8dab327203676c46b5
AR-public <khayyamraja96@gmail.com>
2023-11-22 21:23:21 +0000

    Initiate useEffect to watch for when the play/pause button is pressed.

--- src/TimerComponent.jsx
+++ src/TimerComponent.jsx
@@ -10,23 +10,30 @@ export default function TimerComponent(buttonReadsStart) {
   // Initialise States
   let [totalMinutesRemaining, setTotalMinutesRemaining] = useState(startingNumberofMinutes)
   let [totalSecondsRemaining, setTotalSecondsRemaining] = useState(startingNumberofSeconds)
-  // let [totalMilisecondsRemaining, setTotalMilisecondsRemaining] = useState(startingNumberofMiliseconds)
+  let [totalMilisecondsRemaining, setTotalMilisecondsRemaining] = useState(startingNumberofMiliseconds)
 
-  let totalMilisecondsRemaining = useRef(startingNumberofMiliseconds)
+  // Watch value of buttonReadsStart
+
+  function countdown() { setInterval(() => setTotalMilisecondsRemaining(totalMilisecondsRemaining--), 1000) }
+  function stopCountdown() { clearInterval(countdown) }
+
+  useEffect(() => {
+    buttonReadsStart ? countdown() : stopCountdown()
+  }, [buttonReadsStart]);
+
+  // Calculate Variables 
+  setTotalMinutesRemaining(Math.floor())
+  setTotalSecondsRemaining()
+  setTotalMilisecondsRemaining()
+
 
   // Pad display digits to always show two numbers
   let numberOfSecondsRemainingToDisplay = String(totalSecondsRemaining).padStart(2, '0')
   let numberOfMinutesRemainingToDisplay = String(totalMinutesRemaining).padStart(2, '0')
 
-  // Functions
 
   // useEffect() runs when anything in the DOM re-renders
-  useEffect(() => {
-    const interval = setInterval(() => { (totalMilisecondsRemaining.current = totalMilisecondsRemaining - 1000) }, 1000);
-    setTotalMinutesRemaining(Math.floor(totalMilisecondsRemaining / 60000));
-    setTotalSecondsRemaining(Math.floor(totalMilisecondsRemaining / 1000));
-    return () => clearInterval(interval);
-  }, [totalMilisecondsRemaining, startingNumberofMiliseconds]);
+
 
   return (
     <div className="TimerComponent">
