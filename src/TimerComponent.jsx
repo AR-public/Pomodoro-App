dce8f2fd1c02d3350d65b15938a076cd657c90d5
AR-public <khayyamraja96@gmail.com>
2023-11-22 22:44:05 +0000

    Timer begins counting down when start button is pressed

--- src/TimerComponent.jsx
+++ src/TimerComponent.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 
-export default function TimerComponent(buttonReadsStart) {
+export default function TimerComponent({ buttonReadsStart }) {
 
   // Initialise Variables
   const startingNumberofMinutes = 25
@@ -13,33 +13,43 @@ export default function TimerComponent(buttonReadsStart) {
   let [totalMilisecondsRemaining, setTotalMilisecondsRemaining] = useState(startingNumberofMiliseconds)
 
   // Start/stop countdown when the start/pause button is pressed
-  function countdown() { setInterval(setTotalMilisecondsRemaining(totalMilisecondsRemaining--), 1000) }
-  function stopCountdown() { clearInterval(countdown) }
+  function countdown() {
+    setInterval(() => {
+      setTotalMilisecondsRemaining(totalMilisecondsRemaining = totalMilisecondsRemaining - 1000);
+    }, 1000);
+  }
+
+  function stopCountdown() {
+    clearInterval(countdown);
+  }
 
   useEffect(() => {
-    buttonReadsStart ? stopCountdown() : countdown()
-    setInterval(console.log(totalMilisecondsRemaining), 1000)
+    buttonReadsStart ? stopCountdown() : countdown();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [buttonReadsStart]);
 
   // Calculate Minute/Seconds 
-  function recalculateMinutesToDisplay() { setTotalMinutesRemaining(Math.floor(totalMilisecondsRemaining / 60000)) }
-  function recalculateSecondsToDisplay() { setTotalSecondsRemaining(Math.floor(totalMilisecondsRemaining / 1000)) }
+  function recalculateMinutesToDisplay() {
+    setTotalMinutesRemaining(Math.floor(totalMilisecondsRemaining / 60000));
+  }
+
+  function recalculateSecondsToDisplay() {
+    setTotalSecondsRemaining(Math.floor((totalMilisecondsRemaining % 60000) / 1000));
+  }
 
   useEffect(() => {
-    recalculateMinutesToDisplay()
+    recalculateMinutesToDisplay();
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, totalMilisecondsRemaining);
+  }, [totalMilisecondsRemaining]);
 
   useEffect(() => {
-    recalculateSecondsToDisplay()
+    recalculateSecondsToDisplay();
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, totalMilisecondsRemaining);
+  }, [totalMilisecondsRemaining]);
 
   // Pad display digits to always show two numbers
   let numberOfMinutesRemainingToDisplay = String(totalMinutesRemaining).padStart(2, '0')
   let numberOfSecondsRemainingToDisplay = String(totalSecondsRemaining).padStart(2, '0')
-
 
   return (
     <div className="TimerComponent">
