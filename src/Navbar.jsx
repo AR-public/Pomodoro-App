112f6353a0da5995c8c588e49c587f3116dd4512
AR-public <khayyamraja96@gmail.com>
2024-01-20 20:09:31 +0000

    Update location of images.

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -7,19 +7,19 @@ export default function Navbar() {
     <div className="Navbar">
       <h1>
         <a href="/app" className="NavbarLogoAnchor">
-          <img src="./icon-white2.png" alt="pomofocus icon" className="NavbarLogo" />
+          <img src="./images/icon-white2.png" alt="pomofocus icon" className="NavbarLogo" />
           <div className="NavbarAppTitle">Pomofocus</div>
         </a>
       </h1>
 
       <span className="NavbarButtonSpan">
         <button className="NavbarReportButton">
-          <img src="./graph-white.png" alt="graph icon" className="NavbarReportIcon" />
+          <img src="./images/graph-white.png" alt="graph icon" className="NavbarReportIcon" />
           Report
         </button>
 
         <button className="NavbarSettingsButton">
-          <img src="./config-white.png" alt="Settings icon" className="NavbarSettingsIcon" />
+          <img src="./images/config-white.png" alt="Settings icon" className="NavbarSettingsIcon" />
           Settings
         </button>
       </span>
